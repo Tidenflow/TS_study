d0dbcbdc150b7e0736f66bfdfb23f0ba456232d7
jaten16 <jatenliu16@gmail.com>
2026-03-17 19:12:40 +0800

    test : how ts translation

---
+++ src/01-Basic-Types/primitive.js
@@ -0,0 +1,8 @@
+"use strict";
+const userName = "Tom";
+const age = 25;
+const isStudent = true;
+const sym = Symbol("唯一标识");
+const nullVal = null;
+const undefinedVal = undefined;
+console.log(userName, age, isStudent, sym, nullVal, undefinedVal);
---
+++ src/01-Basic-Types/primitive.ts
@@ -0,0 +1,8 @@
+const userName: string = "Tom";
+const age: number = 25;
+const isStudent: boolean = true;
+const sym: symbol = Symbol("唯一标识");
+const nullVal: null = null;
+const undefinedVal: undefined = undefined;
+
+console.log(userName, age, isStudent, sym, nullVal, undefinedVal);
